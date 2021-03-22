59e389ce134ddc262f86787de5e24dda7b539c66
mdian <strangeman079@gmail.com>
2021-03-22 09:21:40 +0200

    finish

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-let c = document.createElement("ff");
+// let c = document.createElement("ff");
+
+
+let a = [1, 2, 3]
+
+let s = "1,2,3"
+
+console.log(a == s)
+
+
+let card = document.querySelector('.card_order')
+let btnCard = document.querySelector('.btncard')
+console.log(card)
+btnCard.addEventListener("click", () => {
+    card.classList.toggle("show")
+    console.log("hh")
+})
